b0ba3b627b499974bbcf47f6044309b99001efa1
BigRichi <big.richi@outlook.com>
2021-03-08 15:36:27 -0500

    start auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:login, :handle_login]
+
+  def login
+  end
+
+  def handle_login
+
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to event_path(@user)
+    else
+      redirect_to login_path
+    end
+
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get "/login", to: "users#login", as: "login"
+  post "/handle_login", to: 'users#handle_login'
+  
+  delete "/logout", to: "users#logout"
+
   resources :event_categories
   resources :categories
   resources :calendar_dates
   resources :events
   resources :users
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
