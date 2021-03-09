3f51efc9667c363870e03290bba7c13634eeb935
BigRichi <big.richi@outlook.com>
2021-03-09 10:24:18 -0500

    added event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController
+
+    def index
+        @events = @current_user.events
+        @event = Event.new
+        @calendar_dates = CalendarDate.all
+    end
+
+    def new
+        # @event = Event.new
+        # @calendar_dates = CalendarDate.all
+    end
+
+    def create
+        event = Event.create(event_params)
+        redirect_to event_path(event_params[:event_id])
+    end
+    
+
+    private
+     
+    def event_params
+        params.require(:event).permit!
+    end  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to event_path(@user.events)
+      redirect_to events_path
     else
       redirect_to login_path
     end
